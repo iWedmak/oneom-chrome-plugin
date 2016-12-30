6b4d2b17a8d30b3c18c5afa2ffa5b7a999f935de
iWedmak <iwedmak@gmail.com>
2016-12-30 15:20:14 +0300

    New sources, new progress

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
+var app_id=false;
+chrome.management.getAll(function(items) 
+{
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        if(item.type=='packaged_app' && item.name.includes("OneOm"))
+        {
+            app_id=item.id;
+        }
+    }
+});
 chrome.runtime.onMessageExternal.addListener(
     function(request, sender, callback) 
     {
@@ -6,8 +17,6 @@ chrome.runtime.onMessageExternal.addListener(
             var method = request.method ? request.method.toUpperCase() : 'GET';
 
             xhttp.onload = function() {
-                console.log(xhttp.responseText);
-                console.log('some');
                 callback(xhttp.responseText);
             };
             xhttp.onerror = function() {
@@ -16,10 +25,61 @@ chrome.runtime.onMessageExternal.addListener(
                 callback();
             };
             xhttp.open(method, request.url, true);
-            xhttp.responseType = 'text';
+            
+            if(request.type)
+            {
+                xhttp.responseType = request.type;
+            }
+            else
+            {
+                xhttp.responseType = 'text';
+            }
+            if(method=='POST')
+            {
+                xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            }
+            xhttp.send(request.send_array);
+            console.log(request);
+            return true; // prevents the callback from being called too early on return
+        }
+    }
+);
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, callback) 
+    {
+        //*
+        if (request.action == "xhttp") {
+            var xhttp = new XMLHttpRequest();
+            var method = request.method ? request.method.toUpperCase() : 'GET';
+
+            xhttp.onload = function() {
+                callback(xhttp.response);
+            };
+            xhttp.onerror = function() {
+                // Do whatever you want on error. Don't forget to invoke the
+                // callback to clean up the communication port.
+                callback();
+            };
+            xhttp.open(method, request.url, true);
+            
+            if(request.type)
+            {
+                xhttp.responseType = request.type;
+            }
             xhttp.send(request.data);
             return true; // prevents the callback from being called too early on return
         }
+        else if(request.action == "download")
+        {
+            chrome.downloads.download({url:request.url},
+                function(downloadId)
+                {
+                    console.log("download begin, the downId is:" + downloadId);
+                }
+            );
+        }
+        //*/
     }
 );
 
@@ -38,7 +98,20 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     {
         // Modify the headers for these pages
         urls: [
-            "*://*/*"
+            "*://vodlocker.com/*", 
+            "*://vidto.me/*", 
+            "*://vidzi.tv/*", 
+            "*://subsmovies.com/*", 
+            "*://thepiratebay.cr/*", 
+            "*://rutor.org/*", 
+            "*://rutracker.org/*", 
+            "*://bitsnoop.com/*", 
+            "*://torrentz.eu/*", 
+            "*://extratorrent.cc/*", 
+            "*://eztv.ag/*", 
+            "*://kat.cr/*", 
+            "*://rarbg.to/*", 
+            "*://www.opensubtitles.org/*"
         ],
         // In the main window and frames
         types: ["xmlhttprequest"]
--- index.js
+++ index.js
@@ -12,27 +12,60 @@ $( document ).ready(function() {
         plugin= $('.incoming').attr('tpl-get') ;
         } + ')();';
     (document.head||document.documentElement).appendChild(script); //inject in controller what we have plugin on user side
-});
+    
+    $('a#torrent_read').on('click', function() {
+        //$(this).attr('href')
+        var url=$(this).attr('href');
+        console.log(url);
+        /*
+        var magnetURI='magnet:?xt=urn:btih:48d199e6c46b6715802ba1ac3da91fa7e7986a1f&dn=Ballers.2015.S02E05.480p.x264-mSD&tr=udp%3A//tracker.leechers-paradise.org%3A6969&tr=udp%3A//zer0day.ch%3A1337&tr=udp%3A//open.demonii.com%3A1337&tr=udp%3A//tracker.coppersurfer.tk%3A6969&tr=udp%3A//exodus.desync.com%3A6969';
+        
+        var torrentStream = require('torrent-stream');
 
-function search_result_process() {
-    var link_to_parse=$(this).attr('data-url');
-    if(pattern.test(link_to_parse))
-    {
+        var engine = torrentStream(magnetURI);
+
+        engine.on('ready', function() {
+            engine.files.forEach(function(file) {
+                console.log('filename:', file.name);
+                var stream = file.createReadStream();
+                // stream is readable stream to containing the file content
+            });
+        });
+        //*/
+        /*
+        var WebTorrent = require('webtorrent')
+
+        var client = new WebTorrent()
+        console.log('some');
+        
+        client.add(magnetURI, function (torrent) {
+          // Got torrent metadata!
+          console.log('some');
+          console.log('Client is downloading:', torrent.infoHash)
+
+          torrent.files.forEach(function (file) {
+            // Display the file by appending it to the DOM. Supports video, audio, images, and
+            // more. Specify a container element (CSS selector or reference to DOM node).
+            file.appendTo('body')
+          })
+        })
+        //*/
+        //*
         chrome.runtime.sendMessage
         (
+            'bpmfcnmfcbhaclenfiojipblnbcfmgmb',
             {
                 method: 'GET',
-                action: 'xhttp',
-                url: link_to_parse
+                action: 'download',
+                type: 'arraybuffer',
+                url: url
             }, 
-                function(responseText) 
-                {
-                    var video=responseText.match(/<source src="(.*?)" type="video\/mp4">/);
-                    var img=responseText.match(/poster="(.*?)">/);
-                    $('#video_player').attr('data-url', video[1]).attr('data-img', img[1]);
-                }
+            function(responseText) 
+            {
+                console.log(responseText);
+            }
         );
-        
-    }
-    return false;
-}
+        //*/
+        return false;
+    });
+});
